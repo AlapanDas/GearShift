7b8a236099e788e28373d7d7a4c64c571391b56a
Alapan <alapandas333@gmail.com>
2024-05-24 13:30:56 +0530

    Modified

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
+  theme: {
+    colors: {
+      'primary': '#5b5b5b',
+      'onprimary': '#f2f4f3',
+      'secondary': '#c2e812',
+      'alert': '#f6511d',
+      'notif': '00a6ed'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
