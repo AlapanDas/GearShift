88d02183c0d91b952ea11bc74cd3a0c1da169bf8
Aniruddha Mukherjee <ani05jeet.2003@gmail.com>
2024-05-27 16:37:50 +0530

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import SectionList from './components/Account/Sections';
 import SectionData from './components/Account/SectionData';
 import Card from './components/Cards';
 import data from './assets/data.json';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
   return (
     <>
-     
-    
+     <Header/>
+     <div>Hello world</div>
+    <Footer/>
     </>
   );
 }
