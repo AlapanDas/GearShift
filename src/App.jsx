e99c1a1064f8932a0445fb4bbdf73ebc3a2ed8c4
itzarkabrata <arkabrata1292@gmail.com>
2024-05-27 19:36:25 +0530

    App.jsx cleaned before push

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,7 @@ function App() {
 
   return (
     <>
-      <LoginSignup/>
-      {/* <body className='bg-light dark:bg-dark bg-center bg-cover h-screen' >
-        <div className='mx-auto grid 2xl:grid-cols-5 xl:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 max-sm:grid-cols-1 gap-6 p-4 justify-items-center'>
-          <Card />
-        </div>
-      </body> */}
+      
     </>
   );
 }
