1ac534808fa682dad111565187feace0d8ab0bc6
Aniruddha Mukherjee <ani05jeet.2003@gmail.com>
2024-06-14 23:05:48 +0530

    arka chudi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import './App.css';
+import React from 'react';
 import router from './router';
 import { RouterProvider } from "react-router-dom";
+import Index from './index';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import store from './redux/store';
 
 function App() {
 
@@ -17,6 +22,7 @@ function App() {
       </div>
       <RouterProvider router={router} />*/}
       <RouterProvider router={router} />
+
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from './redux/store';
-import { Provider } from 'react-redux';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import router from './router';
+import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <Header />
-    <App/>
-    <Footer />
-  </Provider>
+    <Provider store={store}>
+        <Header />
+        <App/>
+        <Footer />
+    </Provider>
 );
-
